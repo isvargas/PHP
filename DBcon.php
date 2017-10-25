55007da23749fe39c20ae7418371ff637f34f5d9
Ivan S. Vargas <contato@is5.com.br>
2017-10-25 10:59:53 -0200

    Update DBcon.php

--- DBcon.php
+++ DBcon.php
@@ -1,40 +1,40 @@
 <?php
     //Classe base para facilitar o acesso com PDO
     //baseado em: http://php.net/manual/pt_BR/ref.pdo-mysql.php
-	//alterado por: Ivan S. Vargas
-	//30/08/2017
+    //alterado por: Ivan S. Vargas
+    //30/08/2017
     class DBcon
-	{
+    {
         const USERNAME="usuario";
         const PASSWORD="senha";
         const HOST="servidor";
         const DB="database";
 		
-		var $con = null;
+	var $con = null;
 		
-		//Observacoes
-		//self:: para metodos/atributos estaticos
-		//parent:: para metodos/atributos classe pai
-		//$this para metodos/atributos nao-estaticos
+	//Observacoes
+	//self:: para metodos/atributos estaticos
+	//parent:: para metodos/atributos classe pai
+	//$this para metodos/atributos nao-estaticos
 
         public function getConnection()
-		{
-			if ($this->con == null) {
-				$username = self::USERNAME;
-				$password = self::PASSWORD;
-				$host = self::HOST;
-				$db = self::DB;
-				$this->con = new PDO("mysql:dbname=$db;host=$host", $username, $password);
-			}
-            return $this->con;
+	{
+		if ($this->con == null) {
+			$username = self::USERNAME;
+			$password = self::PASSWORD;
+			$host = self::HOST;
+			$db = self::DB;
+			$this->con = new PDO("mysql:dbname=$db;host=$host", $username, $password);
+		}
+            	return $this->con;
         }
 		
         public function run($sql, $args)
-		{
-            $connection = $this->getConnection();
-            $stmt = $connection->prepare($sql);
-            $stmt->execute($args);
-            return $stmt;
+	{
+        	$connection = $this->getConnection();
+            	$stmt = $connection->prepare($sql);
+            	$stmt->execute($args);
+            	return $stmt;
         }
     }
 ?>
