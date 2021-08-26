010d44289d242ff1b142c1d5b88808e2f14bc606
Ivan S. Vargas <contato@is5.com.br>
2021-08-26 11:08:42 -0300

    Update senfilecurl.php

--- senfilecurl.php
+++ senfilecurl.php
@@ -4,15 +4,14 @@
 	//inicializa o curl
 	$ch = curl_init('http://localhost:8080/files/upload');
 	
-	//configura BasicAuth
-	$header = [
-		//'Content-Type: application/json; charset=utf-8',
-		'Authorization: Basic '.base64_encode('user:pass')
+	//configura basic auth
+	$header = [		
+	     'Authorization: Basic '.base64_encode('user:pass')
 	];
 	
 	//cria o curfile
 	$cfile = new CURLFile('D:\teste.txt', 'text/plain', 'teste.txt');
-		
+	
 	//aplica configuracoes
 	curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
